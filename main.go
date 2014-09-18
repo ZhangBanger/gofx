3393acca9edd0553da7eafaa67c7e87c5b262adf
Andy Zhang <signupforandy@gmail.com>
2014-09-18 01:30:11 -0700

    Add basic order submit - just log to blackhole

--- main.go
+++ main.go
@@ -2,12 +2,27 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type Order struct {
+	User     string  `json:"user" binding:"required"`
+	Security string  `json:"security" binding:"required"`
+	Buy      bool    `json:"buy"`
+	Quantity uint32  `json:"quantity" binding:"required"`
+	Price    float64 `json:"price" binding:"required"`
+}
+
 func main() {
 	r := gin.Default()
 	r.POST(
 		"/orders",
 		func(c *gin.Context) {
-			c.String(202, "created")
+			var order Order
+
+			if c.Bind(&order) {
+				c.JSON(202, gin.H{"status": "accepted", "payload": order})
+			} else {
+				c.JSON(400, gin.H{"status": "invalid fields"})
+			}
+
 		},
 	)
 
